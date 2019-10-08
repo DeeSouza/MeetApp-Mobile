6b27082f05f5719a28735052c06361bf0bee35cf
Diego Souza <dee-souza@hotmail.com>
2019-10-08 11:51:20 -0300

    :construction: Page profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,17 +1,124 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Image } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import logo from '~/assets/images/meetapp-logo.png';
 
 import Background from '~/components/Background';
-import Header from '~/components/Header';
 
-import { Container } from './styles';
+import { updateUserRequest } from '~/store/modules/user/actions';
+
+import { Container, Form, FormInput, SubmitButton, Separator } from './styles';
 
 export default function Profile() {
+	const dispatch = useDispatch();
+
+	const loading = useSelector(state => state.user.loading);
+	const profile = useSelector(state => state.user.profile);
+
+	const [name, setName] = useState(profile.name);
+	const [email, setEmail] = useState(profile.email);
+	const [oldPassword, setOldPassword] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+
+	const emailRef = useRef();
+	const oldPasswordRef = useRef();
+	const passwordRef = useRef();
+	const confirmPasswordRef = useRef();
+
+	useEffect(() => {
+		setOldPassword('');
+		setPassword('');
+		setConfirmPassword('');
+	}, [profile]);
+
+	/**
+	 * Submit form to update user
+	 */
+	function handleSubmit() {
+		const data = {
+			name,
+			email,
+			oldPassword,
+			password,
+			confirmPassword,
+		};
+
+		dispatch(updateUserRequest(data));
+	}
+
 	return (
 		<Background>
 			<Container>
-				<Header />
+				<Image source={logo} />
+
+				<Form>
+					<FormInput
+						icon="person"
+						placeholder="Digite seu nome"
+						autoCorrect={false}
+						returnKeyType="next"
+						value={name}
+						onChangeText={setName}
+						onSubmitEditing={() => emailRef.current.focus()}
+					/>
+
+					<FormInput
+						icon="mail-outline"
+						placeholder="Digite seu e-mail"
+						keyboardType="email-address"
+						autoCorrect={false}
+						autoCapitalize="none"
+						returnKeyType="next"
+						ref={emailRef}
+						value={email}
+						onChangeText={setEmail}
+						onSubmitEditing={() => passwordRef.current.focus()}
+					/>
+
+					<Separator />
+
+					<FormInput
+						icon="lock-outline"
+						secureTextEntry
+						placeholder="Digite sua senha atual"
+						ref={oldPasswordRef}
+						value={oldPassword}
+						onChangeText={setOldPassword}
+						returnKeyType="next"
+						onSubmitEditing={() => passwordRef.current.focus()}
+					/>
+
+					<FormInput
+						icon="lock-outline"
+						secureTextEntry
+						placeholder="Digite uma nova senha"
+						ref={passwordRef}
+						value={password}
+						onChangeText={setPassword}
+						returnKeyType="next"
+						onSubmitEditing={() =>
+							confirmPasswordRef.current.focus()
+						}
+					/>
+
+					<FormInput
+						icon="lock-outline"
+						secureTextEntry
+						placeholder="Confirme sua senha"
+						ref={confirmPasswordRef}
+						returnKeyType="send"
+						onSubmitEditing={handleSubmit}
+						value={confirmPassword}
+						onChangeText={setConfirmPassword}
+					/>
+
+					<SubmitButton loading={loading} onPress={handleSubmit}>
+						ATUALIZAR CONTA
+					</SubmitButton>
+				</Form>
 			</Container>
 		</Background>
 	);
@@ -20,12 +127,6 @@ export default function Profile() {
 function IconTab({ tintColor }) {
 	return <Icon name="person" size={20} color={tintColor} />;
 }
-
-Profile.propTypes = {
-	navigation: PropTypes.shape({
-		navigate: PropTypes.func,
-	}).isRequired,
-};
 
 IconTab.propTypes = {
 	tintColor: PropTypes.string.isRequired,
